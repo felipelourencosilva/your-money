59bd05b0411a436b1ac19ada1d6132f03a374e7f
Felipe Lourenço <lipelourencosilva@gmail.com>
2021-05-09 16:47:59 -0300

    Botei os créditos das imagens
    Agora botei os créditos da imagem na home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -16,6 +16,13 @@ const Home = () => {
       <div className="home-image">
             <img src="/image.png" alt="" />
       </div>
+      <div className="photos-links">
+        <p>Images:</p>
+        <ul>
+          <li className="photo-link">figma.com/community/file/890095002328610853</li>
+          <li className="photo-link">figma.com/community/file/968803933274255367</li>
+        </ul>
+      </div>
     </div>
   );
 };
